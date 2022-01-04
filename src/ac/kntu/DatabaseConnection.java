0eb113c8640cdb9ff661659bed46059c1ff1e5bb
ASUS <alitavakkoli80@gmail.com>
2022-01-04 18:38:13 +0330

    [ADD] rowcount method added [ADD] methods for connection and binding added

--- src/ac/kntu/DatabaseConnection.java
+++ src/ac/kntu/DatabaseConnection.java
@@ -29,16 +29,32 @@ public class DatabaseConnection {
     }
 
     //Prepare Statement with Query
-    public void query(String query)
-    {
-        try {
-            this.stmt = this.connection.prepareStatement(query);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            this.error += e.getMessage();
-            this.dbconnected = false;
-        }
+    public void query(String query) throws SQLException {
+        this.stmt = this.connection.prepareStatement(query,
+                ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.CONCUR_UPDATABLE);
     }
 
+    //Execute the Prepared Statement get result set as Array of Objects
+    public ResultSet execute() throws SQLException {
+        return this.stmt.executeQuery();
+    }
 
+    //Get Record Row Count
+    public int rowCount(String tableName) throws SQLException {
+        String countQuery = "SELECT * FROM " + tableName;
+        query(countQuery);
+        ResultSet rs = execute();
+        rs.last();
+        return rs.getRow();
+
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public boolean isDbconnected() {
+        return dbconnected;
+    }
 }
--- src/ac/kntu/Main.java
+++ src/ac/kntu/Main.java
@@ -1,30 +1,19 @@
 package ac.kntu;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        String SQL = "SELECT * FROM account";
 
         DatabaseConnection databaseConnection = new DatabaseConnection();
-        try (Connection conn = databaseConnection.connect();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(SQL)) {
 
-            while (rs.next()) {
-                System.out.println(rs.getString("account_number") + "\t"
-                        + rs.getString("userphone_number") + "\t"
-                        + rs.getString("shaba_number") + "\t"
-                        + rs.getString("balance"));
-
-            }
-
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+        try {
+            int numr = databaseConnection.rowCount("account");
+            System.out.println(numr);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
+
 
     }
 }
